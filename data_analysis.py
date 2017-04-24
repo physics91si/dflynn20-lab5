92952deaafb98bbdaae58f6d17005e46467af432
Donald Flynn <donaldflynn@rescomp-16-313554.stanford.edu>
2017-04-23 19:11:43 -0700

    I have completed the lab

--- data_analysis.py
+++ data_analysis.py
@@ -14,14 +14,18 @@ def main():
     """This function sould call noisy_packet() to get a Gaussian wave
     packet, call clean_data() to apply a low pass filter to the data and
     finally plot the result."""
-    #TODO add your code here
-
+    x = np.arange(-np.pi, np.pi, 0.01)
+    y_values = noisy_packet(x, 5, 1, 0.2)
+    data = clean_data(x, y_values)
+    x = np.delete(x, 0)
+    plt.plot(x, data)
+    plt.show()
 
 def noisy_packet(x_values, k, sigma, noise_amplitude):
     """This function returns a noisy Gaussian wavepacket with wave
     vector k, standard deviation sigma and Gaussian noise of standard
     deviation noise_amplitude."""
-    clean_y = wavepacket(x_values,k,sigma)
+    clean_y = wavepacket(x_values, k, sigma)
     noisy_y = clean_y + noise_amplitude*np.random.randn(len(x_values))
     return noisy_y
 
@@ -30,7 +34,12 @@ def clean_data(x_values,y_values):
     transform on it, filter out the high frequency noise, transform the
     signal back into real space, and return it."""
 
-    pass #TODO add your code here
-    #return y_clean
+    y_sorted = np.fft.rfft(y_values)
+    clean_frequencies = np.fft.rfftfreq(len(x_values), 0.01)
+    for i in range(0, len(clean_frequencies)):
+	if clean_frequencies[i] > 4 :
+	    y_sorted[i] = 0
+    y_clean = np.fft.irfft(y_sorted)
+    return y_clean
 
 main()  # calls your main function
--- trig.py
+++ trig.py
@@ -3,11 +3,25 @@
 #The following statements are used to import numpy and matplotlib.
 import numpy as np
 import matplotlib.pyplot as plt
-
+import scipy.integrate as int
 
 # TODO fill in this function
 def integrate(y, dx):
-    pass
+    return np.sum(y) * dx
 
 # TODO write code here to setup arrays x and y = sin(x) and then plot them.
 # After this is done implement your integrate function above integrate y
+
+def plot_sine():
+    x = np.arange(0, np.pi, 0.01)
+    y = np.sin(x)
+    z = np.cos(x)
+    plt.plot(x, y)
+    plt.plot(x, z)
+    plt.show()
+    print(integrate(y, 0.01))
+    f = lambda w: np.sin(w)
+    print(int.quad(f,0,np.pi))
+
+
+plot_sine()
